ada3affd70579611a7d93443cc54f589a814f2fd
Thiru <sthirukumaran786@gmail.com>
2026-07-23 14:28:47 +0530

    Sprint 1: Initial Spring Boot project setup

---
+++ backend/aiops-backend/src/test/java/com/aiops/backend/AiopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiops.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiopsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
